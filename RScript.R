5459d540b40c936a8eb54b8ac86f3c5aa87f8a57
Gareth-Warwick <gare.unistudy@gmail.com>
2023-11-12 17:39:42 +0000

    1) Created clearer headings and subheadings 2) Insert pre-processing of YELP Academic Data code 3) Load Data

--- RScript.R
+++ RScript.R
@@ -1,12 +1,39 @@
-#Creating an EC349 Assignment 2.0 to link to GitHub
+#1.0 Linking to Git
 
-#Commit Again2
+##1.1Creating an EC349 Assignment 2.0 to link to GitHub
 
-#Setting Working Directory
+##1.2Commit Again2
+
+
+#2.0 Setting up
+
+##2.1 Setting Working Directory
 setwd("/Users/gare.mac/Desktop/Warwick/Y3/EC349/Summative Assignment/EC349 Assignment 2.0")
 
-#Download and Load Tidyverse Package
+##2.2 Download and Load Tidyverse Package
 install.packages("tidyverse")
 library(tidyverse)
+
+##2.3 Set seed to control randomisation
+set.seed(1)
+
+##2.4 Pre-Processing Yelp Academic Data for the Assignment (Code prepared by Love and Nathan)
+
+###2.4.1 Install and load "jsonlite" package
+install.packages("jsonlite")
+library(jsonlite)
+
+###2.4.2 Clear
+cat("\014")  
+rm(list=ls())
+
+
+#3.0 Load Different Data (Code prepared by Love and Nathan)
+business_data <- stream_in(file("yelp_academic_dataset_business.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
+review_data  <- stream_in(file("yelp_academic_dataset_review.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
+checkin_data  <- stream_in(file("yelp_academic_dataset_checkin.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
+user_data <- stream_in(file("yelp_academic_dataset_user.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
+tip_data  <- stream_in(file("yelp_academic_dataset_tip.json")) #note that stream_in reads the json lines (as the files are json lines, not json)
+
 
 
