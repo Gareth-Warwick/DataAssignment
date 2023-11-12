f3bd2d3030f7d4b5b5cee47527c6f85aaf692abe
Gareth-Warwick <gare.unistudy@gmail.com>
2023-11-12 16:52:14 +0000

    1) Set working directory 2) Download tidyverse package 3) Load tidyverse package

--- RScript.R
+++ RScript.R
@@ -1,3 +1,11 @@
 #Creating an EC349 Assignment 2.0 to link to GitHub
 
-#Commit Again
+#Commit Again2
+
+#Setting Working Directory
+setwd("/Users/gare.mac/Desktop/Warwick/Y3/EC349/Summative Assignment/EC349 Assignment 2.0")
+
+#Download and Load Tidyverse Package
+install.packages("tidyverse")
+library(tidyverse)
+
