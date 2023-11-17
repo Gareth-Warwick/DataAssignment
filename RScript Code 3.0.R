1ae54eaaf495b895a1c11a5f594deeeafcb3661e
Gareth-Warwick <gare.unistudy@gmail.com>
2023-11-17 10:48:27 +0000

    First Commit

---
+++ RScript Code 3.0.R
@@ -0,0 +1,28 @@
+#1.0 Linking to Git
+
+##1.1Creating an EC349 Assignment 2.0 to link to GitHub
+
+##1.2Commit Again2
+
+
+#2.0 Setting up
+
+##2.1 Setting Working Directory
+setwd("/Users/gare.mac/Desktop/Warwick/Y3/EC349/Summative Assignment/EC349 Assignment 2.0")
+
+##2.2 Download and Load Tidyverse Package
+install.packages("tidyverse") 
+library(tidyverse)
+
+##2.3 Set seed to control randomisation
+set.seed(1)
+
+##2.4 Pre-Processing Yelp Academic Data for the Assignment (Code prepared by Love and Nathan)
+
+###2.4.1 Install and load "jsonlite" package
+install.packages("jsonlite")
+library(jsonlite)
+
+###2.4.2 Clear
+cat("\014")  
+rm(list=ls())
