c5e537ecc1c2339ffe3700e2d2b54d4447211511
Gareth-Warwick <gare.unistudy@gmail.com>
2023-11-17 11:18:05 +0000

    View Data

--- RScript Code 3.0.R
+++ RScript Code 3.0.R
@@ -25,8 +25,12 @@ rm(list=ls())
 
 ###2.4.2 Load the .RDA data (these are the smaller datasets as I couldn't load the big ones)
 
-####2.4.2.1 Load the dataset "review_data_small"
+####2.4.2.1 Load & view the dataset "review_data_small"
 load(file="/Users/gare.mac/Desktop/Warwick/Y3/EC349/Summative Assignment/Assignment/Small Datasets/yelp_review_small.Rda")
 
 ####2.4.2.1 Load the dataset "user_data_small"
 load(file="/Users/gare.mac/Desktop/Warwick/Y3/EC349/Summative Assignment/Assignment/Small Datasets/yelp_user_small.Rda")
+
+###2.4.3 View the data
+view(review_data_small)
+view(user_data_small)
